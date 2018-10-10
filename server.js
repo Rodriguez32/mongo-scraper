d018db3985024e048acca19f0d5ddfd0167b46ea
Azalea <Rodriguez32.ilda0122@gmail.com>
2018-10-10 10:41:13 -0500

    changes to app.js, index and server

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.use(express.json());
 // Make public a static folder
 app.use(express.static("public"));
 
-// Set Handlebars.
+//Set Handlebars.
 // var exphbs = require("express-handlebars");
 
 // app.engine("handlebars", exphbs({
@@ -41,7 +41,9 @@ app.use(express.static("public"));
 // Database configuration with mongoose
 
 mongoose.connect("mongodb://localhost/mongoscraper", { useNewUrlParser: true });
+// var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 
+// mongoose.connect(MONGODB_URI);
 
 // Show any mongoose errors
 // db.on("error", function(error) {
@@ -55,8 +57,96 @@ mongoose.connect("mongodb://localhost/mongoscraper", { useNewUrlParser: true });
 
 // Routes
 
+//GET requests to render Handlebars pages
+// app.get("/", function(req, res) {
+//   Article.find({"saved": false}, function(error, data) {
+//     var hbsObject = {
+//       article: data
+//     };
+//     console.log(hbsObject);
+//     res.render("home", hbsObject);
+//   });
+// });
 
+// app.get("/saved", function(req, res) {
+//   Article.find({"saved": true}).populate("notes").exec(function(error, articles) {
+//     var hbsObject = {
+//       article: articles
+//     };
+//     res.render("saved", hbsObject);
+//   });
+// });
 
+// A GET route for scraping the New York Times website
+app.get("/scrape", function(req, res) {
+  // First, we grab the body of the html with axios
+  axios.get("https://www.nytimes.com/").then(function(response) {
+    
+    var $ = cheerio.load(response.data);
+
+    
+    $("article h2").each(function(i, element) {
+      
+      var result = {};
+
+      result.title = $(this)
+        .children("a")
+        .text();
+      result.link = $(this)
+        .children("a")
+        .attr("href");
+
+      // Create a new Article using the `result` object built from scraping
+      db.Article.create(result)
+        .then(function(dbArticle) {
+          console.log(dbArticle);
+        })
+        .catch(function(err) {
+          return res.json(err);
+        });
+    });
+    res.send("Scrape Complete");
+  });
+});
+
+// Route for getting all Articles from the db
+app.get("/articles", function(req, res) {
+  db.Article.find({})
+    .then(function(dbArticle) {
+      res.json(dbArticle);
+    })
+    .catch(function(err) {
+      res.json(err);
+    });
+});
+
+// Route for grabbing a specific Article by id, populate it with it's note
+app.get("/articles/:id", function(req, res) {
+ 
+  db.Article.findOne({ _id: req.params.id })
+    .populate("note")
+    .then(function(dbArticle) {
+      res.json(dbArticle);
+    })
+    .catch(function(err) {
+      res.json(err);
+    });
+});
+
+// Route for saving/updating an Article's associated Note
+app.post("/articles/:id", function(req, res) {
+  
+  db.Note.create(req.body)
+    .then(function(dbNote) {
+      return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
+    })
+    .then(function(dbArticle) {
+      res.json(dbArticle);
+    })
+    .catch(function(err) {
+      res.json(err);
+    });
+});
 
 
 // Start the server
